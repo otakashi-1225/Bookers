5c67b50c30b6cef8eb629b5f45a7632931e16165
otakashi <56966251+otakashi-1225@users.noreply.github.com>
2022-12-18 07:32:38 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,20 @@
 class BooksController < ApplicationController
   def create
-    @book  = Book.new(book_params)
-    @books = Book.all
-
-    if @book.save
+    book  = Book.new(book_params)
+    if book.save
       flash[:notice] = "Successfully Created!!"
-      render :index
+      redirect_to book_path(book.id)
     else
       flash[:notice] = "Error invalied Text..."
+      @book = Book.new
+      @books = Book.all
       render :index
     end
   end
 
   def index
     @books = Book.all
+    @book  = Book.new
   end
 
 
@@ -32,17 +33,26 @@ class BooksController < ApplicationController
       redirect_to book_path(book.id)
     else
       flash[:notice] = "Error invalied Text..."
+      @book = Book.find(params[:id])
       render :edit
     end
   end
 
-  def destory
-
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "Successfully Destroy!!"
+      redirect_to books_path
+    else
+      @book = Book.new
+      @books = Book.all
+      render :index
+    end
   end
 
   private
   # ストロングパラメータ
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
